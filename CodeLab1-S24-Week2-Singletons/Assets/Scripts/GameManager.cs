270044cd921893d0c19a2abafef59f7612c45f41
AlleyTerry <art8689@nyu.edu>
2024-02-10 00:37:54 -0500

    comments added
    comments added to directory script

--- CodeLab1-S24-Week2-Singletons/Assets/Scripts/GameManager.cs
+++ CodeLab1-S24-Week2-Singletons/Assets/Scripts/GameManager.cs
@@ -33,11 +33,15 @@ public class GameManager : MonoBehaviour
                 HighScore = score;
             }
         }
+        //high score only gets changed when score is higher than score
+        //instead of in update every frame
     }
 
     private int highScore = 0;
 
     private const string KEY_HIGH_SCORE = "High Score";
+    //where the highScore is saved
+    //call it in start so it loads before getting your first point
 
     int HighScore
     {
@@ -50,6 +54,7 @@ public class GameManager : MonoBehaviour
             {
                 string fileContents = File.ReadAllText(DATA_FULL_HS_FILE_PATH);
                 highScore = Int32.Parse(fileContents);
+                //reads the string as an int
                 
             }
             return highScore;
@@ -94,6 +99,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         DATA_FULL_HS_FILE_PATH = Application.dataPath + DATA_DIR + DATA_HS_FILE;
+        //this file path is where the data will be stored
     }
 
     // Update is called once per frame
@@ -115,6 +121,8 @@ public class GameManager : MonoBehaviour
             targetScore = Mathf.RoundToInt(targetScore + targetScore * 1.5f);
             
         }
+        //hit refresh when making folders / files
+        
     }
 
    
