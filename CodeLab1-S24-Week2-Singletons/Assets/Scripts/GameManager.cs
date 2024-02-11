85b14aafc714eb6c51575af620984670594ec2a9
AlleyTerry <art8689@nyu.edu>
2024-02-11 12:44:54 -0500

    fixed score
    deleted high score

--- CodeLab1-S24-Week2-Singletons/Assets/Scripts/GameManager.cs
+++ CodeLab1-S24-Week2-Singletons/Assets/Scripts/GameManager.cs
@@ -10,76 +10,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    private int score = 0;
+    public int score = 0;
 
     public int targetScore = 3;
     
     public TextMeshProUGUI scoreText;
 
     public GameObject buildingPerson;
-
-    public int Score
-    {
-        get
-        {
-            return score;
-        }
-
-        set
-        {
-            score = value;
-            if (score > HighScore)
-            {
-                HighScore = score;
-            }
-        }
-        //high score only gets changed when score is higher than score
-        //instead of in update every frame
-    }
-
-    private int highScore = 0;
-
-    private const string KEY_HIGH_SCORE = "High Score";
-    //where the highScore is saved
-    //call it in start so it loads before getting your first point
-
-    int HighScore
-    {
-        get
-        {
-            //highScore = PlayerPrefs.GetInt(KEY_HIGH_SCORE);
-            //originally used to save small values
-            
-            if (File.Exists(DATA_FULL_HS_FILE_PATH))
-            {
-                string fileContents = File.ReadAllText(DATA_FULL_HS_FILE_PATH);
-                highScore = Int32.Parse(fileContents);
-                //reads the string as an int
-                
-            }
-            return highScore;
-        }
-        
-        set
-        {
-            highScore = value; //place holder valuable for holding values
-            //PlayerPrefs.SetInt(KEY_HIGH_SCORE, highScore);
-            string fileContent = "" + highScore;
-            
-            if (!Directory.Exists(Application.dataPath + DATA_DIR))
-            {
-                Directory.CreateDirectory((Application.dataPath + DATA_DIR));
-            }
-
-            File.WriteAllText(DATA_FULL_HS_FILE_PATH, fileContent);
-        }
-    }
+    
 
     private int levelNum = 1;
 
-    private const string DATA_DIR = "/Data/";
-    private const string DATA_HS_FILE = "hs.txt";
-    private string DATA_FULL_HS_FILE_PATH;
+
 
 
     void Awake()
@@ -98,25 +40,19 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DATA_FULL_HS_FILE_PATH = Application.dataPath + DATA_DIR + DATA_HS_FILE;
-        //this file path is where the data will be stored
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //deletes the pervious high score
-            PlayerPrefs.DeleteKey((KEY_HIGH_SCORE));
-        }
-        scoreText.text = "Level: " + levelNum  + "\nScore: " + score + "\nHigh Score: " + HighScore;
+
+        scoreText.text = "Level: " + levelNum + "\nScore: " + score;
         
         //when score reaches target score, we go to the next level
         if (score >= targetScore)
         {
             levelNum++;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             Instantiate(buildingPerson);
             targetScore = Mathf.RoundToInt(targetScore + targetScore * 1.5f);
             
